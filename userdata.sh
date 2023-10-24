5cbc6a5391685ded05ddb1f35a2f0d0d910ddd27
chinnapareddy <chinna.aduri@gmail.com>
2023-10-24 08:54:16 -0700

    Terraform DNS Record

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yum install  ansible -y >>/opt/userdata.log
-ansible-pull -i localhost, https://github.com/chinna3107/p1-roboshop-ansible1.git main.yml -e component = ${component} >>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/chinna3107/p1-roboshop-ansible1.git main.yml -e component=rabbitmq &>>/opt/userdata.log
 
