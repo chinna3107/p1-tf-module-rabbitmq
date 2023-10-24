3707d24df491742710b1c18caa64259e3c5f4a35
chinnapareddy <chinna.aduri@gmail.com>
2023-10-24 04:25:54 -0700

    Terraform DNS Record

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "main" {
  ami                     = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
-  subnet_id              = var.subnet_id[0]
+  subnet_id              = var.subnet_ids[0]
   tags                   = merge(local.tags, {Name = "${local.name_prefix}-ec2"})
   user_data              = file("${path.module}/userdata.sh")
 }
