68338c4ed873663cd58c629dd02f6009f9f71451
chinnapareddy <chinna.aduri@gmail.com>
2023-11-18 03:56:00 -0800

    Terraform App

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-yum install  ansible -y >>/opt/userdata.log
+yum install  ansible python3.11-pip.noarch -y >>/opt/userdata.log
+ #pip3.11 install botocare boto3 &>>/opt/userdata.log
+
 ansible-pull -i localhost, -U https://github.com/chinna3107/p1-roboshop-ansible1.git main.yml -e component=rabbitmq &>>/opt/userdata.log
 
